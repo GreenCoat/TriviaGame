c2a47902888a66e2cb352946c60f6d8b6a8be587
GreenCoat <nickscunningham@gmail.com>
2017-12-09 10:57:09 -0600

    Now asks questions randomly, added actual questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,87 +18,89 @@ var canGuess = false;
 //List of questions and answers
 var mythTrivia = [
 {
-	q: "some question",
-	a1: "wrong",
-	a2: "wrong",
-	a3: "wrong",
-	a4: "right",
-	ans: "right"
+	q: "This Greek creature is famous for its single eye, and appears in many legends.",
+	a1: "Cyclops",
+	a2: "Catobelpas",
+	a3: "Cetus",
+	a4: "Carcinos",
+	ans: "Cyclops"
 },
 {
-	q: "some question",
-	a1: "right",
-	a2: "wrong",
-	a3: "wrong",
-	a4: "wrong",
-	ans: "right"
+	q: "This Greek monster was known for demanding sacrifices in the form of maidens chained to a rock until its defeat by the hero Perseus.",
+	a1: "Cetus",
+	a2: "Kraken",
+	a3: "Carcinos",
+	a4: "Cyclops",
+	ans: "Cetus"
 },
 {
-	q: "some question",
-	a1: "wrong",
-	a2: "wrong",
-	a3: "right",
-	a4: "wrong",
-	ans: "right"
+	q: "This legendary Greek monster was once beautiful, but was cursed into a monster whose gaze turns victims to stone.",
+	a1: "Stheno",
+	a2: "Euryale",
+	a3: "Medusa",
+	a4: "Medea",
+	ans: "Medusa"
 },
 {
-	q: "some question",
-	a1: "wrong",
-	a2: "right",
-	a3: "wrong",
-	a4: "wrong",
-	ans: "right"
+	q: "Said to be the product of a chicken egg laid upon by a toad, this creature's gaze turns its victims to stone.",
+	a1: "Gorgon",
+	a2: "Cockatrice",
+	a3: "Basilisk",
+	a4: "Manticore",
+	ans: "Cockatrice"
 },
 {
-	q: "some question",
-	a1: "right",
-	a2: "wrong",
-	a3: "wrong",
-	a4: "wrong",
-	ans: "right"
+	q: "This monster possesses a human-like face, a lion-like body and a scorpion-like tail and feeds on humans.",
+	a1: "Griffin",
+	a2: "Chimera",
+	a3: "Dragon",
+	a4: "Manticore",
+	ans: "Manticore"
 },
 {
-	q: "some question",
-	a1: "wrong",
-	a2: "wrong",
-	a3: "wrong",
-	a4: "right",
-	ans: "right"
+	q: "This abominable crossbreed has the traits of a lion, bird, goat, and serpent.",
+	a1: "Chimera",
+	a2: "Manticore",
+	a3: "Wendigo",
+	a4: "Griffin",
+	ans: "Chimera"
 },
 {
-	q: "some question",
-	a1: "wrong",
-	a2: "wrong",
-	a3: "wrong",
-	a4: "right",
-	ans: "right"
+	q: "This cold weather spirit feeds on humans and is created when a human commits cannibalism.",
+	a1: "Qiqirn",
+	a2: "Baykok",
+	a3: "Piasa",
+	a4: "Wendigo",
+	ans: "Wendigo"
 },
 {
-	q: "some question",
-	a1: "wrong",
-	a2: "wrong",
-	a3: "wrong",
-	a4: "right",
-	ans: "right"
+	q: "This noble creature is a cross between a lion and an eagle.",
+	a1: "Pegasus",
+	a2: "Phoenix",
+	a3: "Griffin",
+	a4: "Roc",
+	ans: "Griffin"
 },
 {
-	q: "some question",
-	a1: "wrong",
-	a2: "wrong",
-	a3: "wrong",
-	a4: "right",
-	ans: "right"
+	q: "This dog-like creature guards the gate to the underworld.",
+	a1: "Cerberus",
+	a2: "Scylla",
+	a3: "Cynocephalus",
+	a4: "Minotaur",
+	ans: "Cerberus"
 },
 {
-	q: "some question",
-	a1: "wrong",
-	a2: "wrong",
-	a3: "wrong",
-	a4: "right",
-	ans: "right"
+	q: "This snake-like creature is famed for its regenerative abilities, if its head is struck off, two more will take its place.",
+	a1: "Basilisk",
+	a2: "Python",
+	a3: "Hydra",
+	a4: "Ladon",
+	ans: "Hydra"
 }];
 
-var questionList = mythTrivia;
+var questionList = $.extend(true, [], mythTrivia);
+
+var currentQuestion;
 
 //Display start button, question field and blank answer buttons
 createButtons();
@@ -151,11 +153,15 @@ function stopTimer(){
 }
 
 function renderQuestion(){
-	$("#textbox").text(questionList[question].q);
-	$("#a1").text(questionList[question].a1);
-	$("#a2").text(questionList[question].a2);
-	$("#a3").text(questionList[question].a3);
-	$("#a4").text(questionList[question].a4);
+	var num = Math.floor(Math.random()*questionList.length);
+	currentQuestion = questionList.splice(num, 1);
+	console.log(currentQuestion);
+	console.log(questionList);
+	$("#textbox").text(currentQuestion[0].q);
+	$("#a1").text(currentQuestion[0].a1);
+	$("#a2").text(currentQuestion[0].a2);
+	$("#a3").text(currentQuestion[0].a3);
+	$("#a4").text(currentQuestion[0].a4);
 }
 
 function nextQuestion(){
@@ -172,13 +178,13 @@ function checkAnswer(event){
 	if(canGuess){
 		canGuess = false;
 		stopTimer();
-		if(event.target.innerHTML == questionList[question].ans){
+		if(event.target.innerHTML == currentQuestion[0].ans){
 			$("#timer").text("Correct!");
 			correct++;
 			question++;
 			setTimeout(nextQuestion, 3000);
 		} else {
-			$("#timer").text("Incorrect!  The correct answer was " + questionList[question].ans + "!");
+			$("#timer").text("Incorrect!  The correct answer was " + currentQuestion[0].ans + "!");
 			question++;
 			setTimeout(nextQuestion, 3000);
 		}
@@ -199,6 +205,7 @@ function gameEnd(){
 	countdown = 10;
 	question = 0;
 	correct = 0;
+	questionList = $.extend(true, [], mythTrivia);
 }
 
 
